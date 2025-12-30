05820c9cfa64e459c3355a90fe28e3e7f4888981
Jlara38 <chuplaxlara@gmail.com>
2025-12-29 20:39:20 -0600

    About Me section done. More styling needed but will come back to fix once the other sections have solidified.

--- src/react_files/AboutMe.jsx
+++ src/react_files/AboutMe.jsx
@@ -1,5 +1,7 @@
-import arknights from '../assets/gifs/arknights-hoolheyak.gif'
 import zena from '../assets/images/zena_cat.jpg'
+import gitHub from '../assets/icons/github-fill.svg'
+import linkedIn from '../assets/icons/linkedin-fill.svg'
+import email from '../assets/icons/mail-fill.svg'
 
 export default function AboutMe(){
     return (
@@ -9,6 +11,30 @@ export default function AboutMe(){
 
                 <div className="name-titles">
                     <h1>Jose Lara</h1>
+                    <h2>Software Engineer</h2>
+
+                    <p>
+                        I'm a fledgling developer with an interest in app development and data handling.<br/>
+                        My interest in software development started with my curiosity on how video games worked <br/>
+                        and how developers where able to create these kinds of experiences for the general public.<br/>
+                        Since then I have studied and now developing to one day reach the heights that many other <br/>
+                        successful developers have reached. 
+                    </p>
+
+                    <div className='icons'>
+                        <a href='https://github.com/Jlara38'>
+                            <img src={gitHub} alt='GitHub'></img>
+                        </a>
+
+                        <a href='https://www.linkedin.com/in/jtlara28/' alt="LinkedIn">
+                            <img src={linkedIn} alt='LinkedIn'></img>
+                        </a>
+
+                        <a href="mailto:josetlara28@gmail.com">
+                            <img src={email} alt='Email'></img>
+                        </a>
+                    </div>
+
                 </div>
 
                 <div className="professional-headshot">
