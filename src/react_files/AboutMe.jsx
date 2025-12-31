9cd7ad62bf37ca884d8c02bc8e09888e3fe4fe3e
Jlara38 <chuplaxlara@gmail.com>
2025-12-30 23:02:00 -0600

    Opted into switching the pure flexbox style into a more gridbox style.

--- src/react_files/AboutMe.jsx
+++ src/react_files/AboutMe.jsx
@@ -5,43 +5,39 @@ import email from '../assets/icons/mail-fill.svg'
 
 export default function AboutMe(){
     return (
-        <div className="aboutme-container">
+        <div className="general-info">
 
-            <div className="general-info">
+            <div className="name-titles">
+                <h1>Jose Lara</h1>
+                <h2>Software Engineer</h2>
 
-                <div className="name-titles">
-                    <h1>Jose Lara</h1>
-                    <h2>Software Engineer</h2>
-
-                    <p>
+                <p>
                         I'm a fledgling developer with an interest in app development and data handling.<br/>
                         My interest in software development started with my curiosity on how video games worked <br/>
                         and how developers where able to create these kinds of experiences for the general public.<br/>
                         Since then I have studied and now developing to one day reach the heights that many other <br/>
                         successful developers have reached. 
-                    </p>
+                </p>
 
-                    <div className='icons'>
-                        <a href='https://github.com/Jlara38'>
-                            <img src={gitHub} alt='GitHub'></img>
-                        </a>
+                <div className='icons'>
+                    <a href='https://github.com/Jlara38'>
+                        <img src={gitHub} alt='GitHub'></img>
+                    </a>
 
-                        <a href='https://www.linkedin.com/in/jtlara28/' alt="LinkedIn">
-                            <img src={linkedIn} alt='LinkedIn'></img>
-                        </a>
+                    <a href='https://www.linkedin.com/in/jtlara28/' alt="LinkedIn">
+                        <img src={linkedIn} alt='LinkedIn'></img>
+                    </a>
 
-                        <a href="mailto:josetlara28@gmail.com">
-                            <img src={email} alt='Email'></img>
-                        </a>
-                    </div>
-
-                </div>
-
-                <div className="professional-headshot">
-                    <img src={zena} alt="A picture of myself" id='picture'></img>
+                    <a href="mailto:josetlara28@gmail.com">
+                         <img src={email} alt='Email'></img>
+                    </a>
                 </div>
             </div>
 
-        </div>
+                <div className="professional-headshot">
+                    <img src={zena} alt="A picture of myself"></img>
+                </div>
+            </div>
+
     )
 }
